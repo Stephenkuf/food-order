07f4c3b37af54454787ff52e977fc819f265d806
Stephenkuf <Stephensleem@gmail.com>
2019-08-20 07:38:38 +0100

    update on food.vue

--- src/views/food.vue
+++ src/views/food.vue
@@ -3,6 +3,10 @@
         <div class="row restaurant-banner">
             
         </div>
+        <div class="row">MENU</div>
+        <div class=" row menu-display">
+
+        </div>
     </div>
 </template>
 
@@ -16,8 +20,12 @@
 <style scoped>
     .restaurant-banner{
         height: 300px;
+        width:100%;
         background-image: url(../assets/nigfood2.jpg);
+        background-size: 100%;  
         border-bottom: 1px solid black;
+        background-repeat:no-repeat;     
+        
     }
 
-</style>
+</style>        
